8ef72807b8ad5e558759d47fbf52734ec6f2563e
P-DOEUN <doeun1101@gmail.com>
2026-03-10 22:36:19 +0900

    [Feat] Add Google Verification Variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GOOGLE_SITE_VERIFICATION?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
