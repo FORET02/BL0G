dcecb1ecbf8da0439f4d5f5c8ff1d33adc481b9a
P-DOEUN <doeun1101@gmail.com>
2026-03-10 23:17:32 +0900

    [Fix} Modify robots.txt.js

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -1,10 +1,14 @@
 import siteConfig from '@config/site';
 
 export async function GET() {
+  const baseUrl = import.meta.env.BASE_URL ?? '/';
+  const normalizedBaseUrl =
+    baseUrl === '/' ? '' : baseUrl.replace(/\/$/, '');
+
   const robots = `User-agent: *
 Allow: /
 
-Sitemap: ${siteConfig.siteUrl}/sitemap-index.xml`;
+Sitemap: ${siteConfig.siteUrl}${normalizedBaseUrl}/sitemap-index.xml`;
 
   return new Response(robots, {
     headers: {
